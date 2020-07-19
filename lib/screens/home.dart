19a4c4842d14b12689c61ac7c2ac3f9219e36d4e
FrameTee29 <teeraphat123@hotmail.com>
2020-07-19 23:50:19 +0700

    Flexible for word-wrap

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -45,17 +45,41 @@ class _HomeState extends State<Home> {
   Widget medical() {
     return Container(
       padding: EdgeInsets.all(30),
-      margin: EdgeInsets.only(top:30),
+      margin: EdgeInsets.only(top: 30),
       child: Column(
         children: <Widget>[
           Row(
-            children: <Widget>[Text("MEDICAL",style: TextStyle(color:Colors.white,fontSize:50,fontWeight: FontWeight.bold),)],
+            children: <Widget>[
+              Text(
+                "MEDICAL",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
           ),
           Row(
-            children: <Widget>[Text('TERMENOLOGY',style: TextStyle(color:Colors.white,fontSize:40,fontWeight: FontWeight.bold),)],
+            children: <Widget>[
+              Text(
+                'TERMENOLOGY',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
           ),
           Row(
-            children: <Widget>[],
+            children: <Widget>[
+              new Flexible(
+                  child: Column(
+                children: <Widget>[
+                  Text(
+                      'รายงานนี้เป็นส่วนหนึ่งของรายวิชา 806 - 415 ฝึกงาน'),
+                ],
+              ))
+            ],
           ),
         ],
       ),
